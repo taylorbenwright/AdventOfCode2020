ec713b9ea1256a3a9ccc3720aa96620a6a16f732
taylor <taylor@straykitestudios.com>
2020-12-07 18:19:43 -0600

    Refactor. This was pretty gross before. I was parsing the input robustly but it made the rest of the code very obtuse

--- lib/day07.py
+++ lib/day07.py
@@ -1,5 +1,59 @@
 """
+--- Day 7: Handy Haversacks ---
+You land at the regional airport in time for your next flight. In fact, it looks like you'll even have time to grab some food: all flights are currently delayed due to issues in luggage processing.
 
+Due to recent aviation regulations, many rules (your puzzle input) are being enforced about bags and their contents; bags must be color-coded and must contain specific quantities of other color-coded bags. Apparently, nobody responsible for these regulations considered how long they would take to enforce!
+
+For example, consider the following rules:
+
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+These rules specify the required contents for 9 bag types. In this example, every faded blue bag is empty, every vibrant plum bag contains 11 bags (5 faded blue and 6 dotted black), and so on.
+
+You have a shiny gold bag. If you wanted to carry it in at least one other bag, how many different bag colors would be valid for the outermost bag? (In other words: how many colors can, eventually, contain at least one shiny gold bag?)
+
+In the above rules, the following options would be available to you:
+
+A bright white bag, which can hold your shiny gold bag directly.
+A muted yellow bag, which can hold your shiny gold bag directly, plus some other bags.
+A dark orange bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+So, in this example, the number of bag colors that can eventually contain at least one shiny gold bag is 4.
+
+How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long; make sure you get all of it.)
+
+--- Part Two ---
+It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+faded blue bags contain 0 other bags.
+dotted black bags contain 0 other bags.
+vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
 """
 
 
@@ -9,52 +63,22 @@ def get_chunks(content_list):
 
 
 input_list = [line.rstrip('.').replace('bags', 'bag').replace(' contain ', ' ').replace(',', '') for line in open("inputs/day7_input.txt", "r").read().splitlines()]
-input_dicts = []
+input_dicts = {}
 for _input in input_list:
     inputs = _input.split(' ')
-    entry_dict = dict()
-    entry_dict['bag type'] = ' '.join(inputs[:3])
-    contents = list(get_chunks(inputs[3:]))
-    entry_dict['contents'] = [{'quant': content[0], 'content type': ' '.join(content[-3:])} for content in contents if content[0] != 'no']
-    input_dicts.append(entry_dict)
-# print(input_dicts)
-
-# all_bags = set([bag_type['bag type'] for bag_type in input_dicts])
-# print(len(all_bags))
+    input_dicts[' '.join(inputs[:3])] = [(int(content[0]), ' '.join(content[-3:])) for content in list(get_chunks(inputs[3:])) if content[0] != 'no']
+print(input_dicts)
 
 
-def search_for_bag_type(bags_list, bag_type, container_bags):
-    """
-
-    :param bags_list:
-    :type bags_list: list[dict]
-    :param bag_type:
-    :type bag_type: str
-    :return: the itr number
-    :rtype:
-    """
-    outer_bags = set()
-    for bag_dict in bags_list:  # type: dict
-        for content in bag_dict['contents']:
-            if content['content type'] == bag_type:
-                outer_bags.add(bag_dict['bag type'])
-    container_bags.update(outer_bags)
-    for outer_bag in outer_bags:
-        container_bags.update(search_for_bag_type(bags_list, outer_bag, container_bags))
-    return container_bags
+def search_for_bag_type(bag_list, bag_name):
+    outers = bag_name in bag_list and any(color == 'shiny gold bag' or search_for_bag_type(bag_list, color) for count, color in bag_list[bag_name])
+    return outers
 
 
-def count_all_inner_bags(bag_dict):
-    itr = 0
-    for content in bag_dict['contents']:
-        print(content)
-        itr += int(content['quant'])
-        inner_bag_dict = [bag_type for bag_type in input_dicts if bag_type['bag type'] == content['content type']][0]
-        itr += count_all_inner_bags(inner_bag_dict) * int(content['quant'])
-    return itr
+def count_all_inner_bags(bag_list, bag_name):
+    return sum([count_all_inner_bags(bag_list, bag) * value for value, bag in bag_list[bag_name]]) + sum([value for value, bag in bag_list[bag_name]])
 
 
-shiny_gold_dict = [shiny_gold for shiny_gold in input_dicts if shiny_gold['bag type'] == 'shiny gold bag'][0]
-print(len(search_for_bag_type(input_dicts, 'shiny gold bag', set())))
-print(count_all_inner_bags(shiny_gold_dict))
+print(sum([search_for_bag_type(input_dicts, bag) for bag in input_dicts]))
+print(count_all_inner_bags(input_dicts, 'shiny gold bag'))
 
