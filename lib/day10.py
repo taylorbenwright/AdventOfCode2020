49b32d305a6ddfd92a416a678a2e64b2c231539a
taylor <taylor@straykitestudios.com>
2020-12-10 13:31:53 -0600

    Day10 refactor and timer

--- lib/day10.py
+++ lib/day10.py
@@ -2,39 +2,41 @@
 
 """
 
-# from time import perf_counter
-# from functools import wraps
-# def timer(func):
-#     @wraps(func)
-#     def wrapper(*args, **kwargs):
-#         start = perf_counter()
-#         ret = func(*args, **kwargs)
-#         print(f"{func.__name__.replace('_', ' ')} took: {perf_counter() - start:.8f} seconds")
-#         return ret
-#     return wrapper
+from itertools import tee
+from time import perf_counter
+from functools import wraps
+
+
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = perf_counter()
+        ret = func(*args, **kwargs)
+        print(f"{func.__name__.replace('_', ' ')} took: {perf_counter() - start:.8f} seconds")
+        return ret
+    return wrapper
+
+
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
 
 
 input_list = sorted([int(line) for line in open("inputs/day10_input.txt", "r").read().splitlines()])
-# input_list.insert(0, 0)
 input_list.append(max(input_list)+3)
-# print(input_list)
 
 
+@timer
 def part01():
-    prev_digit = 0
-    diff = []
-    for ind, digit in enumerate(input_list):
-        diff.append(digit - prev_digit)
-        prev_digit = digit
+    diff = [input_list[0] - 0]
+    for first, second in pairwise(input_list):
+        diff.append(second - first)
     return diff.count(1) * diff.count(3)
 
 
+@timer
 def part02():
-    """
-    2-pointers to check chunks from the end of the list backwards, adding successful chunks each time.
-    :return:
-    :rtype:
-    """
     ptr = 0
     chunk_sum = 0
     valid_paths = [0] * len(input_list)
@@ -47,8 +49,8 @@ def part02():
         valid_paths[ind] = chunk_sum + (digit <= 3)
         chunk_sum += valid_paths[ind]
 
-    return int(valid_paths[-1])
+    return int(valid_paths[-1]), valid_paths
 
 
-# print(part01())
-part02()
+print(part01())
+print(part02())
