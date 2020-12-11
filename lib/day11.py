5a1de85d68239682e2ddeeca0c0a51c9e1415a16
taylor <taylor@straykitestudios.com>
2020-12-11 12:48:49 -0600

    Solution for day 11.

--- lib/day11.py
+++ lib/day11.py
@@ -2,8 +2,6 @@
 
 """
 
-from itertools import chain
-from copy import deepcopy
 from time import perf_counter
 from functools import wraps
 
@@ -18,155 +16,47 @@ def timer(func):
     return wrapper
 
 
-class NonNegativeList(list):
-    def __getitem__(self, item):
-        if item < 0:
-            raise IndexError('...')
-        return list.__getitem__(self, item)
-
-
-inputs = NonNegativeList([NonNegativeList([char for char in line]) for line in open("inputs/day11_input.txt", "r").read().splitlines()])
+inputs = {(x, y): state for y, line in enumerate(open("inputs/day11_input.txt", "r").read().splitlines()) for x, state in enumerate(line)}
 
 EMPTY = 'L'
 FLOOR = '.'
 OCCUPIED = '#'
 
+vector_range = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-def get_occupied_cells(input_list, row, column, skip_empty=False):
+
+def get_occupied_cells(row, column, input_dict, skip_empty=False):
     itr = 0
-
-    ind = 1
-    while True:  # top left
-        try:
-            if input_list[row-ind][column-ind] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
-
-    ind = 1
-    while True:  # top middle
-        try:
-            if input_list[row-ind][column] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
-
-    ind = 1
-    while True:  # top right
-        try:
-            if input_list[row-ind][column+ind] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
-
-    ind = 1
-    while True:  # center left
-        try:
-            if input_list[row][column-ind] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
-
-    ind = 1
-    while True:  # center right
-        try:
-            if input_list[row][column+ind] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
-
-    ind = 1
-    while True:  # bottom left
-        try:
-            if input_list[row+ind][column-ind] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
-
-    ind = 1
-    while True:  # bottom middle
-        try:
-            if input_list[row+ind][column] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
-
-    ind = 1
-    while True:  # bottom right
-        try:
-            if input_list[row+ind][column+ind] == OCCUPIED:
-                itr += 1
-                break
-        except IndexError:
-            break
-        if not skip_empty:
-            break
-        ind += 1
+    for x_vec, y_vec in vector_range:
+        new_x, new_y = row+x_vec, column+y_vec
+        while skip_empty and input_dict.get((new_x, new_y), EMPTY) not in [EMPTY, OCCUPIED]:
+            new_x += x_vec
+            new_y += y_vec
+        itr += input_dict.get((new_x, new_y), EMPTY) == OCCUPIED
 
     return itr
 
 
 @timer
-def fill_seats(input_list, occupied_threshold=4, skip_empty=False):
+def fill_seats(input_dict, occupied_threshold=4, skip_empty=False):
     loops = 0
     while True:
-        buffer_list = deepcopy(input_list)
-        for ind1, row in enumerate(input_list):
-            for ind2, state in enumerate(row):
-                occupied_cells = get_occupied_cells(input_list, ind1, ind2, skip_empty=skip_empty)
-                if state == EMPTY and occupied_cells == 0:
-                    buffer_list[ind1][ind2] = OCCUPIED
-                elif state == OCCUPIED and occupied_cells >= occupied_threshold:
-                    buffer_list[ind1][ind2] = EMPTY
-
-                # if occupied_cells == 0 and state == EMPTY:
-                #     buffer_list[ind1][ind2] = OCCUPIED
-                # elif occupied_cells >= occupied_threshold and state == OCCUPIED:
-                #     buffer_list[ind1][ind2] = EMPTY
-                # print(occupied_cells)
-                # if state == EMPTY:
-                #     if check_empty_rule(occupied_cells):
-                #         buffer_list[ind1][ind2] = OCCUPIED
-                # elif state == OCCUPIED:
-                #     if check_occupied_rule(occupied_cells, 4):
-                #         buffer_list[ind1][ind2] = EMPTY
+        buffer_dict = input_dict.copy()
+        for (row, column), state in input_dict.items():
+            occupied_cells = get_occupied_cells(row, column, input_dict, skip_empty=skip_empty)
+            if state == EMPTY and occupied_cells == 0:
+                buffer_dict[(row, column)] = OCCUPIED
+            if state == OCCUPIED and occupied_cells >= occupied_threshold:
+                buffer_dict[(row, column)] = EMPTY
 
         loops += 1
-        if input_list == buffer_list:
+
+        if input_dict == buffer_dict:
             break
-        input_list = buffer_list
-    return list(chain(*input_list)).count(OCCUPIED)
+        input_dict = buffer_dict
+
+    return list(input_dict.values()).count(OCCUPIED), loops
 
 
 print(fill_seats(inputs))
 print(fill_seats(inputs, occupied_threshold=5, skip_empty=True))
-
-# print(list(chain(*input_list)).count(OCCUPIED), loops)
